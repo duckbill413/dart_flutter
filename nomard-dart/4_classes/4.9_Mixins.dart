e1fcc2338c05b1bcb4ff1ce8a1755c1635539ac8
duckbill413 <uhyeon7399@daum.net>
2023-11-29 03:13:34 +0900

    dart chapter 4 classes

--- nomard-dart/4_classes/4.9_Mixins.dart
+++ nomard-dart/4_classes/4.9_Mixins.dart
@@ -1,17 +1,26 @@
-// 생성자가 없는 클래스
-class Strong {
+// Mixin은 생성자가 없는 클래스여야 한다.
+// class는 mixin class의 property를 단순히 뻇어오기만 한다.
+mixin Strong {
   final double strengthLevel = 1500.99;
 }
 
-class QuickRunner {
+mixin QuickRunner {
   void runQuick() {
     print('ruuuuuuuuuuuuuuuun');
   }
 }
 
+mixin Tall {
+  final double height = 1.99;
+}
+
 enum Team { red, blue }
 
-class Player {
+class Horse with Strong, QuickRunner {}
+
+class Kid with QuickRunner {}
+
+class Player with Strong, QuickRunner, Tall {
   final Team team;
 
   Player({required this.team});
