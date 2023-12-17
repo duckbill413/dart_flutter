afb59197f124221340a3750521f0c775805ccfd0
duckbill413 <uhyeon7399@daum.net>
2023-12-17 16:09:31 +0900

    #6 Webtoon app - 6.5 waitForWebToons

--- webtoon/lib/screens/home_screen.dart
+++ webtoon/lib/screens/home_screen.dart
@@ -1,7 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:webtoon/services/api_service.dart';
 
-class HomeScreen extends StatelessWidget {
+import '../models/webtoon_model.dart';
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<WebtoonModel> webtoons = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    waitForWebtoons();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,15 +26,23 @@ class HomeScreen extends StatelessWidget {
       backgroundColor: Colors.white,
       appBar: AppBar(
         centerTitle: true,
-        title: Text("오늘의 웹툰",
-        style: TextStyle(
-          fontSize: 22,
-          fontWeight: FontWeight.w600,
-        ),),
+        title: Text(
+          "오늘의 웹툰",
+          style: TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
         foregroundColor: Colors.green,
         backgroundColor: Colors.white,
         elevation: 2,
       ),
     );
+  }
+
+  void waitForWebtoons() async {
+    webtoons = await ApiService().getTodayToons();
+    isLoading = false;
+    setState(() {});
   }
 }
