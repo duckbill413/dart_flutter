bef4043284bb57c3b9d55cec98d8589cf11abf3d
duckbill413 <uhyeon7399@daum.net>
2023-12-23 01:10:44 +0900

    #4 Authentication - 4.5 FormButton > AnimatedDefaultTextStyle의 사용 > Stateful Widget의 경우 context는 어디에서든 사용 가능하다. 즉, Route시 BuildContext를 파라미터로 넘겨줄 필요가 없다.

--- tiktok/lib/features/authentication/username_screen.dart
+++ tiktok/lib/features/authentication/username_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok/constants/gaps.dart';
 import 'package:tiktok/constants/sizes.dart';
+import 'package:tiktok/features/authentication/email_screen.dart';
+import 'package:tiktok/features/authentication/widgets/form_button.dart';
 
 class UsernameScreen extends StatefulWidget {
   const UsernameScreen({super.key});
@@ -21,6 +23,19 @@ class _UsernameScreenState extends State<UsernameScreen> {
         _username = _usernameController.text;
       });
     });
+  }
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    super.dispose();
+  }
+
+  void _onNextTap() {
+    if (_username.isEmpty) return;
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (context) => const EmailScreen(),
+    ));
   }
 
   @override
@@ -59,7 +74,7 @@ class _UsernameScreenState extends State<UsernameScreen> {
                 hintText: 'Username',
                 enabledBorder: UnderlineInputBorder(
                     borderSide: BorderSide(
-                  color: Colors.grey.shade400,
+                      color: Colors.grey.shade400,
                 )),
                 focusedBorder: UnderlineInputBorder(
                   borderSide: BorderSide(
@@ -70,27 +85,9 @@ class _UsernameScreenState extends State<UsernameScreen> {
               cursorColor: Theme.of(context).primaryColor,
             ),
             Gaps.v16,
-            FractionallySizedBox(
-              widthFactor: 1,
-              child: AnimatedContainer(
-                // MEMO: Container의 변화에 애니메이션 효과 적용
-                padding: const EdgeInsets.symmetric(
-                  vertical: Sizes.size14,
-                ),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(Sizes.size5),
-                  color: _username.isEmpty
-                      ? Colors.grey.shade300
-                      : Theme.of(context).primaryColor,
-                ),
-                duration: const Duration(milliseconds: 300),
-                child: const Text('Next',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w600,
-                    )),
-              ),
+            FormButton(
+              disabled: _username.isEmpty,
+              onTap: _onNextTap,
             ),
           ],
         ),
