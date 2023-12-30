3e32c39fc33a01e544237ffbede941bb3ae133dc
duckbill413 <uhyeon7399@daum.net>
2023-12-30 23:42:13 +0900

    # Tab Navigation - 6.5 Custom NavigationBar

--- tiktok/lib/features/main_navigation/main_navigation_screen.dart
+++ tiktok/lib/features/main_navigation/main_navigation_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok/constants/sizes.dart';
+import 'package:tiktok/features/main_navigation/widgets/nav_tab.dart';
 
 class MainNavigationScreen extends StatefulWidget {
-  MainNavigationScreen({super.key});
+  const MainNavigationScreen({super.key});
 
   @override
   State<MainNavigationScreen> createState() => _MainNavigationScreenState();
@@ -38,6 +40,49 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: Sizes.size8,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              NavigationTab(
+                text: 'Home',
+                isSelected: _selectedIndex == 0,
+                icon: FontAwesomeIcons.house,
+                onTap: () => _onTap(0),
+              ),
+              NavigationTab(
+                text: 'Discover',
+                isSelected: _selectedIndex == 1,
+                icon: FontAwesomeIcons.magnifyingGlass,
+                onTap: () => _onTap(1),
+              ),
+              NavigationTab(
+                text: 'Inbox',
+                isSelected: _selectedIndex == 3,
+                icon: FontAwesomeIcons.message,
+                onTap: () => _onTap(3),
+              ),
+              NavigationTab(
+                text: 'Profile',
+                isSelected: _selectedIndex == 4,
+                icon: FontAwesomeIcons.user,
+                onTap: () => _onTap(4),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  /// IOS bottom Navigation bar main.dart의 Material theme를 Cupertino로 변경 필요
+  CupertinoTabScaffold CupertinoBottomNavigationVer3() {
     return CupertinoTabScaffold(
       tabBar: CupertinoTabBar(
         items: const [
@@ -55,6 +100,7 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
     );
   }
 
+  /// Android google NavigationBar using Material Theme ver 3
   Scaffold MaterialScaffoldVer2() {
     return Scaffold(
       body: _screens[_selectedIndex],
@@ -81,7 +127,7 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
           NavigationDestination(
             icon: FaIcon(
               FontAwesomeIcons.apple,
-              color: Colors.purple,
+              color: Colors.purple, // ignore: non_constant_identifier_names
             ),
             label: 'House',
           ),
@@ -90,44 +136,48 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
     );
   }
 
-  BottomNavigationBar BottomNavigationBarVer() {
-    return BottomNavigationBar(
-      // type: BottomNavigationBarType.shifting,
-      // selectedItemColor: Theme.of(context).primaryColor,
-      currentIndex: _selectedIndex,
-      onTap: _onTap,
-      items: const [
-        BottomNavigationBarItem(
-          icon: FaIcon(FontAwesomeIcons.house),
-          label: "Home",
-          tooltip: 'What are you',
-          backgroundColor: Colors.amber,
-        ),
-        BottomNavigationBarItem(
-          icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
-          label: "Search",
-          tooltip: 'What are you',
-          backgroundColor: Colors.blue,
-        ),
-        BottomNavigationBarItem(
-          icon: FaIcon(FontAwesomeIcons.house),
-          label: "Home",
-          tooltip: 'What are you',
-          backgroundColor: Colors.pink,
-        ),
-        BottomNavigationBarItem(
-          icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
-          label: "Search",
-          tooltip: 'What are you',
-          backgroundColor: Colors.lightGreen,
-        ),
-        BottomNavigationBarItem(
-          icon: FaIcon(FontAwesomeIcons.house),
-          label: "Home",
-          tooltip: 'What are you',
-          backgroundColor: Colors.deepPurpleAccent,
-        ),
-      ],
+  /// Android google NavigationBar using Material Theme ver 2
+  Scaffold MaterialScaffoldVer3() {
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        // type: BottomNavigationBarType.shifting,
+        // selectedItemColor: Theme.of(context).primaryColor,
+        currentIndex: _selectedIndex,
+        onTap: _onTap,
+        items: const [
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.house),
+            label: "Home",
+            tooltip: 'What are you',
+            backgroundColor: Colors.amber,
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
+            label: "Search",
+            tooltip: 'What are you',
+            backgroundColor: Colors.blue,
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.house),
+            label: "Home",
+            tooltip: 'What are you',
+            backgroundColor: Colors.pink,
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
+            label: "Search",
+            tooltip: 'What are you',
+            backgroundColor: Colors.lightGreen,
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.house),
+            label: "Home",
+            tooltip: 'What are you',
+            backgroundColor: Colors.deepPurpleAccent,
+          ),
+        ],
+      ),
     );
   }
 }
