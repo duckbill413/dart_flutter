3f3a1f4455f2ffb09f3961aed026f6efef3e28ba
duckbill413 <uhyeon7399@daum.net>
2024-01-02 00:22:31 +0900

    # Video Timeline - 7.5 AnimationController > AnimationController을 이용해서 play 버튼의 크기 변경 방법 1 > ```dart > _animationController = AnimationController( >   vsync: this, >   lowerBound: 1.0, >   upperBound: 1.5, >   value: 1.5, >   duration: _animationDuration, > ); > _animationController.addListener(() { >   setState(() {}); > }); > ```

--- tiktok/lib/features/videos/widgets/video_post.dart
+++ tiktok/lib/features/videos/widgets/video_post.dart
@@ -18,9 +18,13 @@ class VideoPost extends StatefulWidget {
   State<VideoPost> createState() => _VideoPostState();
 }
 
-class _VideoPostState extends State<VideoPost> {
+class _VideoPostState extends State<VideoPost>
+    with SingleTickerProviderStateMixin {
   final VideoPlayerController _videoPlayerController =
-  VideoPlayerController.asset("assets/videos/tiktok_video_001.mp4");
+      VideoPlayerController.asset("assets/videos/tiktok_video_001.mp4");
+  late final AnimationController _animationController;
+  final Duration _animationDuration = const Duration(milliseconds: 200);
+  bool _isPaused = false;
 
   void _onVideoChange() {
     if (_videoPlayerController.value.isInitialized) {
@@ -44,6 +48,17 @@ class _VideoPostState extends State<VideoPost> {
   void initState() {
     super.initState();
     _initVideoPlayer();
+
+    _animationController = AnimationController(
+      vsync: this,
+      lowerBound: 1.0,
+      upperBound: 1.5,
+      value: 1.5,
+      duration: _animationDuration,
+    );
+    _animationController.addListener(() {
+      setState(() {});
+    });
   }
 
   @override
@@ -61,9 +76,14 @@ class _VideoPostState extends State<VideoPost> {
   void _onTogglePause() {
     if (_videoPlayerController.value.isPlaying) {
       _videoPlayerController.pause();
+      _animationController.reverse();
     } else {
       _videoPlayerController.play();
+      _animationController.forward();
     }
+    setState(() {
+      _isPaused = !_isPaused;
+    });
   }
 
   @override
@@ -77,21 +97,28 @@ class _VideoPostState extends State<VideoPost> {
             child: _videoPlayerController.value.isInitialized
                 ? VideoPlayer(_videoPlayerController)
                 : Container(
-              color: Colors.black,
-            ),
+                    color: Colors.black,
+                  ),
           ),
           Positioned.fill(
             child: GestureDetector(
               onTap: _onTogglePause,
             ),
           ),
-          const Positioned.fill(
+          Positioned.fill(
             child: IgnorePointer(
-              child: Center(
-                child: FaIcon(
-                  FontAwesomeIcons.play,
-                  color: Colors.white,
-                  size: Sizes.size52,
+              child: Transform.scale(
+                scale: _animationController.value,
+                child: AnimatedOpacity(
+                  opacity: _isPaused ? 1 : 0,
+                  duration: _animationDuration,
+                  child: const Center(
+                    child: FaIcon(
+                      FontAwesomeIcons.play,
+                      color: Colors.white,
+                      size: Sizes.size52,
+                    ),
+                  ),
                 ),
               ),
             ),
