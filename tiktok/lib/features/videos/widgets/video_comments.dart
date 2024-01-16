1c853cc99044da65e42b591a2a4a95c634f10189
duckbill413 <uhyeon7399@daum.net>
2024-01-16 23:53:01 +0900

    #8 Comments Screen - 8.4 Conclusions > - 스크롤바 추가 > - write comment 위에 여백 추가

--- tiktok/lib/features/videos/widgets/video_comments.dart
+++ tiktok/lib/features/videos/widgets/video_comments.dart
@@ -11,6 +11,7 @@ class VideoComment extends StatefulWidget {
 }
 
 class _VideoCommentState extends State<VideoComment> {
+  final ScrollController _scrollController = ScrollController();
   bool _isWriting = false;
 
   void _onClosePressed() {
@@ -55,63 +56,69 @@ class _VideoCommentState extends State<VideoComment> {
           onTap: () => _stopCommenting(),
           child: Stack(
             children: [
-              ListView.separated(
-                padding: const EdgeInsets.symmetric(
-                  vertical: Sizes.size10,
-                  horizontal: Sizes.size16,
-                ),
-                itemCount: 10,
-                itemBuilder: (context, index) => Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    CircleAvatar(
-                      radius: 18,
-                      backgroundColor: Colors.grey.shade500,
-                      foregroundColor: Colors.white,
-                      foregroundImage: const NetworkImage(
-                          'https://avatars.githubusercontent.com/u/86183856?v=4'),
-                      child: const Text('오리너굴'),
-                    ),
-                    Gaps.h10,
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+              Scrollbar(
+                controller: _scrollController,
+                child: ListView.separated(
+                  controller: _scrollController,
+                  padding: const EdgeInsets.only(
+                    top: Sizes.size10,
+                    left: Sizes.size16,
+                    right: Sizes.size16,
+                    bottom: Sizes.size96 + Sizes.size20,
+                  ),
+                  itemCount: 10,
+                  itemBuilder: (context, index) => Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      CircleAvatar(
+                        radius: 18,
+                        backgroundColor: Colors.grey.shade500,
+                        foregroundColor: Colors.white,
+                        foregroundImage: const NetworkImage(
+                            'https://avatars.githubusercontent.com/u/86183856?v=4'),
+                        child: const Text('오리너굴'),
+                      ),
+                      Gaps.h10,
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'duckbill',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: Sizes.size14,
+                                color: Colors.grey.shade500,
+                              ),
+                            ),
+                            Gaps.v3,
+                            const Text(
+                              "That's not it I've seen the same thing but also in a cave",
+                            ),
+                          ],
+                        ),
+                      ),
+                      Gaps.h10,
+                      Column(
                         children: [
+                          FaIcon(
+                            FontAwesomeIcons.heart,
+                            color: Colors.grey.shade500,
+                            size: Sizes.size20,
+                          ),
+                          Gaps.v2,
                           Text(
-                            'duckbill',
+                            '52.2K',
                             style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: Sizes.size14,
                               color: Colors.grey.shade500,
                             ),
                           ),
-                          Gaps.v3,
-                          const Text(
-                            "That's not it I've seen the same thing but also in a cave",
-                          ),
                         ],
                       ),
-                    ),
-                    Gaps.h10,
-                    Column(
-                      children: [
-                        FaIcon(
-                          FontAwesomeIcons.heart,
-                          color: Colors.grey.shade500,
-                          size: Sizes.size20,
-                        ),
-                        Gaps.v2,
-                        Text(
-                          '52.2K',
-                          style: TextStyle(
-                            color: Colors.grey.shade500,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
+                    ],
+                  ),
+                  separatorBuilder: (context, index) => Gaps.v12,
                 ),
-                separatorBuilder: (context, index) => Gaps.v12,
               ),
               Positioned(
                 width: deviceSize.width,
