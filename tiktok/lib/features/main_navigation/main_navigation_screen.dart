5fb3ff7f7ab5606f752785d7716f511c85a94359
duckbill413 <uhyeon7399@daum.net>
2024-01-20 23:29:13 +0900

    #9 Discover - 9.1 Light Navigation > - change main navigation color

--- tiktok/lib/features/main_navigation/main_navigation_screen.dart
+++ tiktok/lib/features/main_navigation/main_navigation_screen.dart
@@ -61,7 +61,8 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
         )
       ]),
       bottomNavigationBar: BottomAppBar(
-        color: Colors.black,
+        color: _selectedIndex == 0 ? Colors.black : Colors.white,
+        surfaceTintColor: _selectedIndex == 0 ? Colors.black : Colors.white,
         child: Padding(
           padding: const EdgeInsets.symmetric(
             horizontal: Sizes.size8,
@@ -76,6 +77,7 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
                 icon: FontAwesomeIcons.house,
                 selectedIcon: FontAwesomeIcons.house,
                 onTap: () => _onTap(0),
+                selectedIndex: _selectedIndex,
               ),
               NavigationTab(
                 text: 'Discover',
@@ -83,10 +85,14 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
                 icon: FontAwesomeIcons.magnifyingGlass,
                 selectedIcon: FontAwesomeIcons.solidCompass,
                 onTap: () => _onTap(1),
+                selectedIndex: _selectedIndex,
               ),
               Gaps.h24,
               GestureDetector(
-                child: PostVideoButton(onTap: _onPostVideoButtonTap),
+                child: PostVideoButton(
+                  onTap: _onPostVideoButtonTap,
+                  inverted: _selectedIndex != 0,
+                ),
               ),
               Gaps.h24,
               NavigationTab(
@@ -95,6 +101,7 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
                 icon: FontAwesomeIcons.message,
                 selectedIcon: FontAwesomeIcons.solidMessage,
                 onTap: () => _onTap(3),
+                selectedIndex: _selectedIndex,
               ),
               NavigationTab(
                 text: 'Profile',
@@ -102,6 +109,7 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
                 icon: FontAwesomeIcons.user,
                 selectedIcon: FontAwesomeIcons.solidUser,
                 onTap: () => _onTap(4),
+                selectedIndex: _selectedIndex,
               ),
             ],
           ),
