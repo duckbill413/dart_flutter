f64c09e27ba62b068079fd0b68c6e1d7e0169178
duckbill <uhyeon7399@daum.net>
2024-12-31 01:52:03 +0900

    4.2 AuthButton - font_awesome_flutter 10.3.0 - Stack - Align

--- tiktok/lib/features/authentication/widgets/auth_button.dart
+++ tiktok/lib/features/authentication/widgets/auth_button.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/src/fa_icon.dart';
 import 'package:tiktok/constants/sizes.dart';
 
 class AuthButton extends StatelessWidget {
   final String text;
+  final FaIcon icon;
 
   const AuthButton({
     super.key,
     required this.text,
+    required this.icon,
   });
 
   @override
@@ -14,21 +17,29 @@ class AuthButton extends StatelessWidget {
     return FractionallySizedBox(
       widthFactor: 1, // 상대적 크기를 이용하여 할당 (0~1)
       child: Container(
-        padding: EdgeInsets.symmetric(
-          vertical: Sizes.size14,
-        ),
+        padding: EdgeInsets.all(Sizes.size14),
         decoration: BoxDecoration(
           border: Border.all(
             color: Colors.grey.shade300,
             width: Sizes.size1,
           ),
         ),
-        child: Text(
-          text,
-          style: TextStyle(
-            fontSize: Sizes.size16,
-            fontWeight: FontWeight.w600,
-          ),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: icon,
+            ),
+            Text(
+              text,
+              style: TextStyle(
+                fontSize: Sizes.size16,
+                fontWeight: FontWeight.w600,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ],
         ),
       ),
     );
