c0f97ff8f57351bb241e34e96f2a23586e638e8e
duckbill <uhyeon7399@daum.net>
2024-12-31 23:02:52 +0900

    4.3 Sign Up Form

--- tiktok/lib/features/authentication/email_screen.dart
+++ tiktok/lib/features/authentication/email_screen.dart
@@ -1,44 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:tiktok/constants/gaps.dart';
-import 'package:tiktok/constants/sizes.dart';
 
-class EmailScreen extends StatelessWidget {
-  const EmailScreen({super.key});
+class UsernameScreen extends StatelessWidget {
+  const UsernameScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: const Text('Sign up'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: Sizes.size36,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Gaps.v40,
-            Text(
-              "Create username",
-              style: TextStyle(
-                fontWeight: FontWeight.w700,
-                fontSize: Sizes.size24,
-                color: Colors.black,
-              ),
-            ),
-            Gaps.v8,
-            Text(
-              "You can always change this later.",
-              style: TextStyle(
-                fontSize: Sizes.size16,
-                color: Colors.black54,
-              ),
-            )
-          ],
-        ),
-      ),
-    );
+    return const Placeholder();
   }
 }
--- tiktok/lib/features/authentication/username_screen.dart
+++ tiktok/lib/features/authentication/username_screen.dart
@@ -1,10 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok/constants/gaps.dart';
+import 'package:tiktok/constants/sizes.dart';
 
 class UsernameScreen extends StatelessWidget {
   const UsernameScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text('Sign up'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: Sizes.size36,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Gaps.v40,
+            Text(
+              "Create username",
+              style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: Sizes.size24,
+                color: Colors.black,
+              ),
+            ),
+            Gaps.v8,
+            Text(
+              "You can always change this later.",
+              style: TextStyle(
+                fontSize: Sizes.size16,
+                color: Colors.black54,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
