27400b31490c73136b640653cb77344d811c6a85
duckbill <uhyeon7399@daum.net>
2024-12-31 23:29:07 +0900

    4.4 Username Screen

--- tiktok/lib/features/authentication/username_screen.dart
+++ tiktok/lib/features/authentication/username_screen.dart
@@ -2,8 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:tiktok/constants/gaps.dart';
 import 'package:tiktok/constants/sizes.dart';
 
-class UsernameScreen extends StatelessWidget {
+class UsernameScreen extends StatefulWidget {
   const UsernameScreen({super.key});
+
+  @override
+  State<UsernameScreen> createState() => _UsernameScreenState();
+}
+
+class _UsernameScreenState extends State<UsernameScreen> {
+  final TextEditingController _usernameController = TextEditingController();
+
+  String _username = "";
+
+  @override
+  void initState() {
+    super.initState();
+    _usernameController.addListener(() {
+      setState(() {
+        _username = _usernameController.text;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,7 +54,53 @@ class UsernameScreen extends StatelessWidget {
                 fontSize: Sizes.size16,
                 color: Colors.black54,
               ),
-            )
+            ),
+            Gaps.v16,
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(
+                hintText: "Username",
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey.shade400,
+                  ),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey.shade400,
+                  ),
+                ),
+              ),
+              cursorColor: Theme.of(context).primaryColor,
+            ),
+            Gaps.v28,
+            FractionallySizedBox(
+              widthFactor: 1,
+              child: AnimatedContainer(
+                padding: EdgeInsets.symmetric(
+                  vertical: Sizes.size16,
+                ),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(
+                    Sizes.size5,
+                  ),
+                  color: _username.isEmpty
+                      ? Colors.grey.shade400
+                      : Theme.of(context).primaryColor,
+                ),
+                duration: Duration(
+                  milliseconds: 300,
+                ),
+                child: Text(
+                  "Next",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
           ],
         ),
       ),
