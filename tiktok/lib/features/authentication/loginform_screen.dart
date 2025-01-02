181b3676debb7bba9136433266950dcbe3f3f4fc
duckbill <uhyeon7399@daum.net>
2025-01-03 00:21:23 +0900

    4.9 Login Form - Form - TextFormField - GlobalKey (validation)

--- tiktok/lib/features/authentication/loginform_screen.dart
+++ tiktok/lib/features/authentication/loginform_screen.dart
@@ -1,10 +1,100 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok/constants/gaps.dart';
+import 'package:tiktok/constants/sizes.dart';
+import 'package:tiktok/features/authentication/widgets/form_button.dart';
 
-class LoginFormScreen extends StatelessWidget {
+class LoginFormScreen extends StatefulWidget {
   const LoginFormScreen({super.key});
 
   @override
+  State<LoginFormScreen> createState() => _LoginFormScreenState();
+}
+
+class _LoginFormScreenState extends State<LoginFormScreen> {
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
+
+  final Map<String, String> formData = {};
+
+  void _onSubmitTap() {
+    if (_formkey.currentState != null) {
+      if (_formkey.currentState!.validate()) {
+        _formkey.currentState!.save();
+      }
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Log in"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: Sizes.size36,
+        ),
+        child: Form(
+          key: _formkey,
+          child: Column(
+            children: [
+              Gaps.v28,
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Email",
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                ),
+                validator: (value) {
+                  return null;
+                },
+                onSaved: (newValue) {
+                  if (newValue != null) {
+                    formData['email'] = newValue;
+                  }
+                },
+              ),
+              Gaps.v16,
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Password",
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                ),
+                validator: (value) {
+                  return null;
+                },
+                onSaved: (newValue) {
+                  if (newValue != null) {
+                    formData['password'] = newValue;
+                  }
+                },
+              ),
+              Gaps.v28,
+              FormButton(
+                disabled: false,
+                onTap: _onSubmitTap,
+                text: "Login",
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
