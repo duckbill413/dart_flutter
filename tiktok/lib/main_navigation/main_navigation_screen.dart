06ceb11a16d7a1cf884dca3614c189774e30d65a
duckbill <uhyeon7399@daum.net>
2025-01-08 02:03:55 +0900

    #6.2 BottomNavigationBar - BottomNavigationBar 에서 4개 이상의 BottomNavigationBarItem 의 배경색을 모두 설정할 경우 버튼 배경색이 존재한다.

--- tiktok/lib/main_navigation/main_navigation_screen.dart
+++ tiktok/lib/main_navigation/main_navigation_screen.dart
@@ -1,10 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class MainNavigationScreen extends StatelessWidget {
+class MainNavigationScreen extends StatefulWidget {
   const MainNavigationScreen({super.key});
 
   @override
+  State<MainNavigationScreen> createState() => _MainNavigationScreenState();
+}
+
+class _MainNavigationScreenState extends State<MainNavigationScreen> {
+  int _selectedIndex = 0;
+
+  final screens = [
+    Center(
+      child: Text("Home"),
+    ),
+    Center(
+      child: Text("Search"),
+    ),
+    Center(
+      child: Text("Home"),
+    ),
+    Center(
+      child: Text("Search"),
+    ),
+    Center(
+      child: Text("Home"),
+    ),
+  ];
+
+  void _onTap(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        // selectedItemColor: Theme.of(context).primaryColor,
+        type: BottomNavigationBarType.shifting,
+        currentIndex: _selectedIndex,
+        onTap: _onTap,
+        items: [
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.house),
+            label: "Home",
+            tooltip: "What are you?",
+            backgroundColor: Colors.red,
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
+            label: "Search",
+            tooltip: "What are you?",
+            backgroundColor: Colors.orange,
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.house),
+            label: "Home",
+            tooltip: "What are you?",
+            backgroundColor: Colors.green,
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
+            label: "Search",
+            tooltip: "What are you?",
+            backgroundColor: Colors.blue,
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.house),
+            label: "Home",
+            tooltip: "What are you?",
+            backgroundColor: Colors.purple,
+          ),
+        ],
+      ),
+    );
   }
 }
