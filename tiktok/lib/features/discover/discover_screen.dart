cdc600441fde4a210563aea0d744cb815436314f
duckbill <uhyeon7399@daum.net>
2025-01-29 22:16:00 +0900

    #9.4 Grid Item - AspectRatio -- 이미지의 비율을 강제로 조정 - FadeInImage -- 이미지가 로딩되기 전까지 기본 이미지를 보여주고 로딩되면 fade in 되면서 로딩된 이미지를 출력 - DefaultTextStyle -- DefaultTextStyle 의 하위에 있는 위젯에 대하여 TextStyle 을 통일

--- tiktok/lib/features/discover/discover_screen.dart
+++ tiktok/lib/features/discover/discover_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok/constants/gaps.dart';
 import 'package:tiktok/constants/sizes.dart';
 
 final tabs = [
@@ -43,32 +45,90 @@ class DiscoverScreen extends StatelessWidget {
             ],
           ),
         ),
-        body: TabBarView(children: [
-          GridView.builder(
-            itemCount: 20,
-            padding: EdgeInsets.all(
-              Sizes.size6,
-            ),
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              crossAxisSpacing: Sizes.size10,
-              mainAxisSpacing: Sizes.size10,
-              childAspectRatio: 9 / 16,
-            ),
-            itemBuilder: (context, index) => Container(
-              color: Colors.teal,
-            ),
-          ),
-          for (var tab in tabs.skip(1))
-            Center(
-              child: Text(
-                tab,
-                style: TextStyle(
-                  fontSize: Sizes.size28,
-                ),
+        body: TabBarView(
+          children: [
+            GridView.builder(
+              itemCount: 20,
+              padding: EdgeInsets.all(
+                Sizes.size6,
               ),
-            )
-        ]),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: Sizes.size10,
+                mainAxisSpacing: Sizes.size10,
+                childAspectRatio: 9 / 20,
+              ),
+              itemBuilder: (context, index) => Column(
+                children: [
+                  AspectRatio(
+                    aspectRatio: 9 / 16,
+                    child: FadeInImage.assetNetwork(
+                      placeholder: "assets/images/picture1.jpg",
+                      image:
+                          "https://images.unsplash.com/photo-1737143765999-bd3be790ab4f?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8",
+                      fit: BoxFit.cover,
+                      placeholderFit: BoxFit.cover,
+                    ),
+                  ),
+                  Gaps.v10,
+                  Text(
+                    "This is a very long caption for my tiktok that im upload just now currently.",
+                    style: TextStyle(
+                      fontSize: Sizes.size16 + Sizes.size2,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  Gaps.v4,
+                  DefaultTextStyle(
+                    style: TextStyle(
+                      color: Colors.grey.shade600,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    child: Row(
+                      children: [
+                        CircleAvatar(
+                          radius: 12,
+                          backgroundImage: NetworkImage(
+                            "https://avatars.githubusercontent.com/u/86183856?v=4",
+                          ),
+                        ),
+                        Gaps.h4,
+                        Expanded(
+                          child: Text(
+                            "My avatar is going to be very long.",
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                        Gaps.h4,
+                        FaIcon(
+                          FontAwesomeIcons.heart,
+                          size: Sizes.size16,
+                          color: Colors.grey.shade600,
+                        ),
+                        Gaps.h2,
+                        Text(
+                          "2.5M",
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            for (var tab in tabs.skip(1))
+              Center(
+                child: Text(
+                  tab,
+                  style: TextStyle(
+                    fontSize: Sizes.size28,
+                  ),
+                ),
+              )
+          ],
+        ),
       ),
     );
   }
