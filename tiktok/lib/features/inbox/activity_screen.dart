3fe39048a0b8820584c9552a334ced23e8a0b486
duckbill <uhyeon7399@daum.net>
2025-02-02 12:25:57 +0900

    #10.4 RotationTransition - AnimationController, SingleTickerProviderStateMixin - Animation, Tween - Animation.animate -> AnimationController - RotationTransition

--- tiktok/lib/features/inbox/activity_screen.dart
+++ tiktok/lib/features/inbox/activity_screen.dart
@@ -10,11 +10,32 @@ class ActivityScreen extends StatefulWidget {
   State<ActivityScreen> createState() => _ActivityScreenState();
 }
 
-class _ActivityScreenState extends State<ActivityScreen> {
+class _ActivityScreenState extends State<ActivityScreen>
+    with SingleTickerProviderStateMixin {
+  // this 나 다른 instance member 를 참조하기 위해선 late 를 이용해서 초기화해 주어야 함
+  late final AnimationController _animationController = AnimationController(
+    vsync: this,
+    duration: Duration(
+      milliseconds: 200,
+    ),
+  );
+  late final Animation<double> _animation = Tween(
+    begin: 0.0,
+    end: -0.5,
+  ).animate(_animationController);
+
   final List<String> _notifications = List.generate(
     20,
     (index) => "${index}h",
   );
+
+  void _onTitleTap() {
+    if (_animationController.isCompleted) {
+      _animationController.reverse();
+    } else {
+      _animationController.forward();
+    }
+  }
 
   void _onDismissed(String notification) {
     _notifications.remove(notification);
@@ -22,10 +43,32 @@ class _ActivityScreenState extends State<ActivityScreen> {
   }
 
   @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("All activity"),
+        title: GestureDetector(
+          onTap: _onTitleTap,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("All activity"),
+              Gaps.h2,
+              RotationTransition(
+                turns: _animation,
+                child: FaIcon(
+                  FontAwesomeIcons.chevronDown,
+                  size: Sizes.size14,
+                ),
+              )
+            ],
+          ),
+        ),
       ),
       body: ListView(
         children: [
