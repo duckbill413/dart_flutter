9537173fcfe98150dbdb574d898ace4a3868e361
duckbill <uhyeon7399@daum.net>
2025-02-11 00:50:50 +0900

    #12.2 SliverAppBar - SliverFixedExtentList - SliverChildBuilderDelegate - Align

--- tiktok/lib/features/users/user_profile_screen.dart
+++ tiktok/lib/features/users/user_profile_screen.dart
@@ -8,18 +8,23 @@ class UserProfileScreen extends StatelessWidget {
     return CustomScrollView(
       slivers: [
         SliverAppBar(
-          floating: true,
-          stretch: true,
+          // 화면의 스크롤이 내려간 상태에서 화면을 약간 끌어올리면 AppBar 가 표시됨
+          // floating: true,
+          // 화면의 상단에 AppBar 가 표시됨
           // pinned: true,
+          // AppBar 가 stretch 될 수 있음
+          // stretch: true,
+          // snap & floating 화면의 스크롤이 내려간 상태에서 약간만 올려도 모든 AppBar 가 floating 됨
+          snap: true,
+          floating: true,
           backgroundColor: Colors.teal,
-          elevation: 1,
           collapsedHeight: 80,
           expandedHeight: 200,
           flexibleSpace: FlexibleSpaceBar(
             stretchModes: [
               StretchMode.blurBackground,
-              // StretchMode.fadeTitle,
               StretchMode.zoomBackground,
+              StretchMode.fadeTitle,
             ],
             background: Image.asset(
               "assets/images/picture2.jpg",
@@ -27,6 +32,19 @@ class UserProfileScreen extends StatelessWidget {
             ),
             title: Text("Hello!"),
           ),
+        ),
+        SliverFixedExtentList(
+          delegate: SliverChildBuilderDelegate(
+            childCount: 50,
+            (context, index) => Container(
+              color: Colors.amber[100 * (index % 9)],
+              child: Align(
+                alignment: Alignment.center,
+                child: Text("Item $index"),
+              ),
+            ),
+          ),
+          itemExtent: 100,
         ),
       ],
     );
