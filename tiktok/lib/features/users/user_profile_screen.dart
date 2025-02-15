67bc4ec156501be50ad985ad34c50ca6f0523d15
duckbill <uhyeon7399@daum.net>
2025-02-16 01:39:35 +0900

    #12.6 TabBar - SliverToBoxAdapter 안에 스크롤이 가능한 위젯인 GridView 를 생성해 주었을때 각각 별개의 스크롤 상태를 가진다. 따라서 사용자 경험에 좋지 못하다.

--- tiktok/lib/features/users/user_profile_screen.dart
+++ tiktok/lib/features/users/user_profile_screen.dart
@@ -9,115 +9,267 @@ class UserProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: [
-        SliverAppBar(
-          title: Text(
-            "duckbill",
-          ),
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: FaIcon(
-                FontAwesomeIcons.gear,
-                size: Sizes.size20,
-              ),
-            )
-          ],
-        ),
-        SliverToBoxAdapter(
-          child: Column(
-            children: [
-              CircleAvatar(
-                radius: 30,
-                foregroundColor: Colors.blue,
-                foregroundImage: NetworkImage(
-                    "https://avatars.githubusercontent.com/u/86183856?v=4"),
-                child: Text("duckbill"),
-              ),
-              Gaps.v20,
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "@duckbill",
-                    style: TextStyle(
-                      fontWeight: FontWeight.w600,
-                      fontSize: Sizes.size18,
-                    ),
-                  ),
-                  Gaps.h5,
-                  FaIcon(
-                    FontAwesomeIcons.solidCircleCheck,
-                    size: Sizes.size16,
-                    color: Colors.blue.shade400,
-                  ),
-                ],
-              ),
-              Gaps.v24,
-              SizedBox(
-                height: Sizes.size48,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    UserStatsCardWidget(
-                      text: "Following",
-                      number: 37,
-                    ),
-                    VerticalDivider(
-                      width: Sizes.size32,
-                      thickness: Sizes.size1,
-                      color: Colors.grey.shade300,
-                      indent: Sizes.size14,
-                      endIndent: Sizes.size14,
-                    ),
-                    UserStatsCardWidget(
-                      number: 1053183,
-                      text: "Followers",
-                    ),
-                    VerticalDivider(
-                      width: Sizes.size32,
-                      thickness: Sizes.size1,
-                      color: Colors.grey.shade300,
-                      indent: Sizes.size14,
-                      endIndent: Sizes.size14,
-                    ),
-                    UserStatsCardWidget(
-                      number: 7914,
-                      text: "Likes",
-                    ),
-                  ],
-                ),
-              ),
-              Gaps.v14,
-              FractionallySizedBox(
-                widthFactor: 0.33,
-                child: Container(
-                  padding: EdgeInsets.symmetric(
-                    vertical: Sizes.size12,
-                  ),
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).primaryColor,
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(
-                        Sizes.size4,
-                      ),
-                    ),
-                  ),
-                  child: Text(
-                    "Follow",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w600,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
+    return DefaultTabController(
+      length: 2,
+      child: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            title: Text(
+              "duckbill",
+            ),
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: FaIcon(
+                  FontAwesomeIcons.gear,
+                  size: Sizes.size20,
                 ),
               )
             ],
           ),
-        )
-      ],
+          SliverToBoxAdapter(
+            child: Column(
+              children: [
+                CircleAvatar(
+                  radius: 30,
+                  foregroundColor: Colors.blue,
+                  foregroundImage: NetworkImage(
+                      "https://avatars.githubusercontent.com/u/86183856?v=4"),
+                  child: Text("duckbill"),
+                ),
+                Gaps.v20,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "@duckbill",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: Sizes.size18,
+                      ),
+                    ),
+                    Gaps.h5,
+                    FaIcon(
+                      FontAwesomeIcons.solidCircleCheck,
+                      size: Sizes.size16,
+                      color: Colors.blue.shade400,
+                    ),
+                  ],
+                ),
+                Gaps.v24,
+                SizedBox(
+                  height: Sizes.size48,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      UserStatsCardWidget(
+                        text: "Following",
+                        number: 37,
+                      ),
+                      VerticalDivider(
+                        width: Sizes.size32,
+                        thickness: Sizes.size1,
+                        color: Colors.grey.shade300,
+                        indent: Sizes.size14,
+                        endIndent: Sizes.size14,
+                      ),
+                      UserStatsCardWidget(
+                        number: 1053183,
+                        text: "Followers",
+                      ),
+                      VerticalDivider(
+                        width: Sizes.size32,
+                        thickness: Sizes.size1,
+                        color: Colors.grey.shade300,
+                        indent: Sizes.size14,
+                        endIndent: Sizes.size14,
+                      ),
+                      UserStatsCardWidget(
+                        number: 7914,
+                        text: "Likes",
+                      ),
+                    ],
+                  ),
+                ),
+                Gaps.v14,
+                FractionallySizedBox(
+                  widthFactor: 0.33,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(
+                      vertical: Sizes.size12,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(
+                          Sizes.size4,
+                        ),
+                      ),
+                    ),
+                    child: Text(
+                      "Follow",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+                Gaps.v14,
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: Sizes.size32,
+                  ),
+                  child: Text(
+                    "All highlights and where to watch live matched on duckbill... All highlights and where to watch live matched on duckbill...",
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Gaps.v14,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    FaIcon(
+                      FontAwesomeIcons.link,
+                      size: Sizes.size12,
+                    ),
+                    Gaps.h4,
+                    Text(
+                      "https://github.com/duckbill413",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                      ),
+                    )
+                  ],
+                ),
+                Gaps.v20,
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.symmetric(
+                      horizontal: BorderSide(
+                        color: Colors.grey.shade200,
+                        width: 0.5,
+                      ),
+                    ),
+                  ),
+                  child: TabBar(
+                    labelColor: Colors.black,
+                    labelPadding: EdgeInsets.symmetric(
+                      vertical: Sizes.size10,
+                    ),
+                    indicatorColor: Colors.black,
+                    indicatorSize: TabBarIndicatorSize.label,
+                    tabs: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: Sizes.size16,
+                        ),
+                        child: Icon(Icons.grid_4x4_rounded),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: Sizes.size16,
+                        ),
+                        child: FaIcon(FontAwesomeIcons.heart),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height,
+                  child: TabBarView(children: [
+                    GridView.builder(
+                      keyboardDismissBehavior:
+                          ScrollViewKeyboardDismissBehavior.onDrag,
+                      itemCount: 20,
+                      padding: EdgeInsets.all(
+                        Sizes.size6,
+                      ),
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: Sizes.size10,
+                        mainAxisSpacing: Sizes.size10,
+                        childAspectRatio: 9 / 20,
+                      ),
+                      itemBuilder: (context, index) => Column(
+                        children: [
+                          Container(
+                            clipBehavior: Clip.hardEdge,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(Sizes.size4),
+                            ),
+                            child: AspectRatio(
+                              aspectRatio: 9 / 16,
+                              child: FadeInImage.assetNetwork(
+                                placeholder: "assets/images/picture1.jpg",
+                                image:
+                                    "https://images.unsplash.com/photo-1737143765999-bd3be790ab4f?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8",
+                                fit: BoxFit.cover,
+                                placeholderFit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          Gaps.v10,
+                          Text(
+                            "This is a very long caption for my tiktok that im upload just now currently.",
+                            style: TextStyle(
+                              fontSize: Sizes.size16 + Sizes.size2,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          Gaps.v4,
+                          DefaultTextStyle(
+                            style: TextStyle(
+                              color: Colors.grey.shade600,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            child: Row(
+                              children: [
+                                CircleAvatar(
+                                  radius: 12,
+                                  backgroundImage: NetworkImage(
+                                    "https://avatars.githubusercontent.com/u/86183856?v=4",
+                                  ),
+                                ),
+                                Gaps.h4,
+                                Expanded(
+                                  child: Text(
+                                    "My avatar is going to be very long.",
+                                    maxLines: 1,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                ),
+                                Gaps.h4,
+                                FaIcon(
+                                  FontAwesomeIcons.heart,
+                                  size: Sizes.size16,
+                                  color: Colors.grey.shade600,
+                                ),
+                                Gaps.h2,
+                                Text(
+                                  "2.5M",
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    Center(
+                      child: Text(
+                        "Page two",
+                      ),
+                    )
+                  ]),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
