f7e31f5c9a75f62d91a438274dfc028e09af496e
duckbill <uhyeon7399@daum.net>
2025-02-24 22:31:50 +0900

    #13.1 AboutListTile - ListTile > showAboutDialog - AboutListTile 위의 두 위젯을 사용함으로써 앱 출시등의 상황에 필요한 오픈소스 라이선스에 대한 고지를 쉽게할 수 있음

--- tiktok/lib/features/settings/settings_screen.dart
+++ tiktok/lib/features/settings/settings_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+// #13.0 ListWheelScrollView
 // 닫기 버튼을 쉽게 만들 수 있는 위젯
 // CloseButton(),
 
@@ -8,6 +9,23 @@ import 'package:flutter/material.dart';
 // CircularProgressIndicator()
 // OS 에 따라서 다른 로딩 상태바 제공
 // CircularProgressIndicator.adaptive()
+
+// 13.1 AboutListTile
+// ListTile(
+//   // 앱 배포시 필요한 오픈소스 라이선스 고지를 쉽게할 수 있음
+//   onTap: () => showAboutDialog(
+//     context: context,
+//     applicationVersion: "1.0",
+//     applicationLegalese: "All rights reserved. please don't copy me",
+//   ),
+//   title: Text(
+//     "About",
+//     style: TextStyle(
+//       fontWeight: FontWeight.w600,
+//     ),
+//   ),
+//   subtitle: Text("About this app...."),
+// ),
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
@@ -18,25 +36,20 @@ class SettingsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text("Settings"),
       ),
-      body: ListView(
-        children: [
-          ListTile(
-            // 앱 배포시 필요한 오픈소스 라이선스 고지를 쉽게할 수 있음
-            onTap: () => showAboutDialog(
-              context: context,
-              applicationVersion: "1.0",
-              applicationLegalese: "All rights reserved. please don't copy me",
-            ),
-            title: Text(
-              "About",
-              style: TextStyle(
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-            subtitle: Text("About this app...."),
+      body: ListTile(
+        // 앱 배포시 필요한 오픈소스 라이선스 고지를 쉽게할 수 있음
+        onTap: () => showAboutDialog(
+          context: context,
+          applicationVersion: "1.0",
+          applicationLegalese: "All rights reserved. please don't copy me",
+        ),
+        title: Text(
+          "About",
+          style: TextStyle(
+            fontWeight: FontWeight.w600,
           ),
-          AboutListTile()
-        ],
+        ),
+        subtitle: Text("About this app...."),
       ),
     );
   }
