1ba53c4b6b402d918406e796616af658bfda4928
duckbill <uhyeon7399@daum.net>
2025-03-01 23:58:38 +0900

    #14.3 MediaQuery
    Widget of the week.
    https://www.youtube.com/watch?v=A3WrA4zAaPw

--- tiktok/lib/features/discover/discover_screen.dart
+++ tiktok/lib/features/discover/discover_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok/constants/breakpoints.dart';
 import 'package:tiktok/constants/gaps.dart';
 import 'package:tiktok/constants/sizes.dart';
 
@@ -76,6 +77,8 @@ class _DiscoverScreenState extends State<DiscoverScreen>
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+
     return DefaultTabController(
       length: tabs.length,
       child: Scaffold(
@@ -193,7 +196,7 @@ class _DiscoverScreenState extends State<DiscoverScreen>
                 Sizes.size6,
               ),
               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
+                crossAxisCount: width > Breakpoints.lg ? 5 : 2,
                 crossAxisSpacing: Sizes.size10,
                 mainAxisSpacing: Sizes.size10,
                 childAspectRatio: 9 / 20,
