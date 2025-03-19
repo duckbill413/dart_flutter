06929060f651e00a95a45d1a7322fef961120f14
duckbill <uhyeon7399@daum.net>
2025-03-20 01:46:49 +0900

    #17.3 pushNamed

--- tiktok/lib/features/settings/sample/sample_list_wheel_scroll_view.dart
+++ tiktok/lib/features/settings/sample/sample_list_wheel_scroll_view.dart
@@ -29,6 +29,5 @@ class SampleListWheelScrollView extends StatelessWidget {
           )
       ],
     );
-    ;
   }
 }
--- tiktok/lib/generated/l10n.dart
+++ tiktok/lib/generated/l10n.dart
@@ -139,7 +139,7 @@ class S {
     final String valueString = valueNumberFormat.format(value);
 
     return Intl.message(
-      '$valueString',
+      valueString,
       name: 'likeCount',
       desc: 'Anything you want',
       args: [valueString],
@@ -154,7 +154,7 @@ class S {
     final String valueString = valueNumberFormat.format(value);
 
     return Intl.message(
-      '$valueString',
+      valueString,
       name: 'commentCount',
       desc: 'Anything you want',
       args: [valueString],
