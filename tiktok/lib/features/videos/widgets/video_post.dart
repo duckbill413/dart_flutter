e0a05c87ee485be91d59d04afbaeb85404b1b8d2
duckbill413 <uhyeon7399@daum.net>
2025-04-17 23:50:51 +0900

    #20.5 InheritedWidget
    Flutter 위젯 트리에서 쉽게 값을 전달할 수 있는 Inherited Widget 생성

--- tiktok/lib/features/videos/widgets/video_post.dart
+++ tiktok/lib/features/videos/widgets/video_post.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok/common/widgets/video_config/video_config.dart';
 import 'package:tiktok/constants/gaps.dart';
 import 'package:tiktok/constants/sizes.dart';
 import 'package:tiktok/features/videos/widgets/video_button.dart';
@@ -251,6 +252,19 @@ class _VieState extends State<VideoPost> with SingleTickerProviderStateMixin {
                   ),
                 )
               ],
+            ),
+          ),
+          Positioned(
+            left: 20,
+            top: 40,
+            child: IconButton(
+              onPressed: () {},
+              icon: FaIcon(
+                VideoConfig.of(context).autoMute
+                    ? FontAwesomeIcons.volumeOff
+                    : FontAwesomeIcons.volumeHigh,
+                color: Colors.white,
+              ),
             ),
           ),
           Positioned(
