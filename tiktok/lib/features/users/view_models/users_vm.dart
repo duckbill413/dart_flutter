cb600f55281b9d03de3771739b2c07837803d0bd
duckbill413 <uhyeon7399@daum.net>
2025-05-02 01:38:55 +0900

    #25.3 createProfile

--- tiktok/lib/features/users/view_models/users_vm.dart
+++ tiktok/lib/features/users/view_models/users_vm.dart
@@ -3,10 +3,14 @@ import 'dart:async';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tiktok/features/users/models/user_profile_model.dart';
+import 'package:tiktok/features/users/repos/user_repo.dart';
 
 class UsersViewModel extends AsyncNotifier<UserProfileModel> {
+  late final UserRepository _userRepository;
+
   @override
   FutureOr<UserProfileModel> build() {
+    _userRepository = ref.read(userRepository);
     return UserProfileModel.empty();
   }
 
@@ -14,16 +18,16 @@ class UsersViewModel extends AsyncNotifier<UserProfileModel> {
     if (credential.user == null) {
       throw Exception("Account not created");
     }
-
-    state = AsyncValue.data(
-      UserProfileModel(
-        uid: credential.user!.uid,
-        email: credential.user!.email ?? "anonymous@anon.com",
-        name: credential.user!.displayName ?? "Anon",
-        bio: "undefined",
-        link: "undefined",
-      ),
+    state = AsyncValue.loading();
+    final profile = UserProfileModel(
+      uid: credential.user!.uid,
+      email: credential.user!.email ?? "anonymous@anon.com",
+      name: credential.user!.displayName ?? "Anon",
+      bio: "undefined",
+      link: "undefined",
     );
+    await _userRepository.createProfile(profile);
+    state = AsyncValue.data(profile);
   }
 }
 
