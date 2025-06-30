0b2410caa355da7cafc6b3cb255671a9b248dd2f
duckbill413 <uhyeon7399@daum.net>
2025-06-30 22:16:00 +0900

    MessageModel 변경

--- tiktok/lib/features/inbox/models/message_model.dart
+++ tiktok/lib/features/inbox/models/message_model.dart
@@ -1,16 +1,19 @@
 class MessageModel {
   final String text;
   final String userId;
+  final int createdAt;
 
   MessageModel({
     required this.text,
     required this.userId,
+    required this.createdAt,
   });
 
   Map<String, dynamic> toMap() {
     return {
       'text': this.text,
       'userId': this.userId,
+      'createdAt': this.createdAt,
     };
   }
 
@@ -18,6 +21,7 @@ class MessageModel {
     return MessageModel(
       text: map['text'] as String,
       userId: map['userId'] as String,
+      createdAt: map['createdAt'] as int,
     );
   }
 }
