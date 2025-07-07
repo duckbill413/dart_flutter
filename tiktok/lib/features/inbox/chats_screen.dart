b9e80d349abf22a1aadeeef203c3ac8b04523a0a
duckbill413 <uhyeon7399@daum.net>
2025-07-08 02:26:40 +0900

    채팅방 목록 화면 수정

--- tiktok/lib/features/inbox/chats_screen.dart
+++ tiktok/lib/features/inbox/chats_screen.dart
@@ -7,6 +7,8 @@ import 'package:tiktok/features/inbox/chat_detail_screen.dart';
 import 'package:tiktok/features/inbox/invite_chat_screen.dart';
 import 'package:tiktok/features/inbox/models/chat_room_model.dart';
 import 'package:tiktok/features/inbox/view_models/chatroom_view_model.dart';
+import 'package:tiktok/features/users/models/user_profile_model.dart';
+import 'package:tiktok/features/users/view_models/users_vm.dart';
 
 class ChatsScreen extends ConsumerStatefulWidget {
   static const String routeName = 'chats';
@@ -66,37 +68,52 @@ class _ChatsScreenState extends ConsumerState<ChatsScreen> {
   }
 
   Widget _makeTile(ChatRoomModel chatRoom, int index) {
+    var personB =
+        ref.watch(usersProvider.notifier).fetchProfile(chatRoom.personB);
     return ListTile(
       key: ValueKey(chatRoom.id),
       onTap: () => _onChatTap(chatRoom.id),
       onLongPress: () => _deleteItem(chatRoom, index),
-      leading: CircleAvatar(
-        radius: 30,
-        foregroundImage: NetworkImage(
-          "https://avatars.githubusercontent.com/u/86183856?v=4",
-        ),
-        child: Text(
-          chatRoom.id.length >= 2 ? chatRoom.id.substring(0, 2) : chatRoom.id,
-        ),
+      leading: FutureBuilder(
+        future: personB,
+        builder:
+            (BuildContext context, AsyncSnapshot<UserProfileModel> snapshot) {
+          if (!snapshot.hasData) {
+            return CircularProgressIndicator.adaptive();
+          }
+
+          return CircleAvatar(
+            radius: 30,
+            foregroundImage: NetworkImage(
+              "${snapshot.data!.avatarLink}",
+            ),
+            child: Text(
+              chatRoom.id.length >= 2
+                  ? chatRoom.id.substring(0, 2)
+                  : chatRoom.id,
+            ),
+          );
+        },
       ),
       title: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.end,
         children: [
           Text(
-            "Chat Room ($index)",
+            "${chatRoom.id}",
             style: const TextStyle(fontWeight: FontWeight.w600),
+            overflow: TextOverflow.ellipsis,
           ),
-          Text(
-            chatRoom.regDt.toString().split(".")[0],
-            style: TextStyle(
-              fontSize: Sizes.size12,
-              color: Colors.grey.shade500,
-            ),
-          ),
+          // Text(
+          //   chatRoom.regDt.toString().split(".")[0],
+          //   style: TextStyle(
+          //     fontSize: Sizes.size12,
+          //     color: Colors.grey.shade500,
+          //   ),
+          // ),
         ],
       ),
-      subtitle: const Text("Don't forget to make video!"),
+      subtitle: Text(chatRoom.personB),
     );
   }
 
